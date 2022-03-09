c12e6336fed21c1c31359ef8c7f10f86bdc9a46e
Edward Foulds <ed2494@googlemail.com>
2022-03-09 12:36:57 +0000

    App.js updated and Home component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home';
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
