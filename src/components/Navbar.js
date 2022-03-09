dbc0637c2586b04e6b808af74df9c30316f220a4
Elise La Rooy <eliselarooy@gmail.com>
2022-03-09 12:38:03 +0000

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand"></div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
