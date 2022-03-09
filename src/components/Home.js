3f50821f669a66bd2370cb06dc7e436aa11b1cdf
Edward Foulds <ed2494@googlemail.com>
2022-03-09 14:34:36 +0000

    Home-page updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,53 @@
 import React from 'react';
 
+const initialUserSearch = {
+  event: '',
+  date: '',
+  location: '',
+};
+
 function Home() {
-  return <p>Home Page</p>;
+  const [userSearch, setUserSearch] = React.useState(initialUserSearch);
+
+  function handleSearchChange(e) {
+    setUserSearch({ ...userSearch, [e.target.name]: e.target.value });
+  }
+
+  console.log(userSearch);
+
+  return (
+    <section className='hero is-fullheight-with-navbar is-warning'>
+      <div className='hero-body'>
+        <div className='container'>
+          <h1 className='title has-text-centered'>Events Home Page</h1>
+        </div>
+
+        <div className='container columns'>
+          <input
+            type='text'
+            placeholder='Search for event'
+            name='event'
+            value={userSearch.event}
+            onChange={handleSearchChange}
+          />
+          <input
+            type='date'
+            name='date'
+            value={userSearch.date}
+            onChange={handleSearchChange}
+          />
+          <input
+            type='search'
+            placeholder='Location'
+            name='location'
+            value={userSearch.location}
+            onChange={handleSearchChange}
+          />
+          <input type='submit' />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
