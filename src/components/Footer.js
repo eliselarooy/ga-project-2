19551b6ec8195f2308a7198dd1c094e88e4b24c3
Elise La Rooy <eliselarooy@gmail.com>
2022-03-11 11:45:20 +0000

    footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,15 @@ const Footer = () => {
     <footer className="footer">
       <hr />
       <div className="content has-text-centered">
-        <p>Made by us</p>
+        <p>
+          Created by Edward and Elise. Open in{' '}
+          <a href="https://github.com/eliselarooy/ga-project-2">
+            <span className="icon">
+              <i className="fab fa-github"></i>
+            </span>
+            <span>Github.</span>
+          </a>
+        </p>
       </div>
     </footer>
   );
