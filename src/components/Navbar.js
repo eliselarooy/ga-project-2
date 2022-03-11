b8508f687906e0733c37b4e2cc3d94dc40042c57
Edward Foulds <ed2494@googlemail.com>
2022-03-11 12:30:48 +0000

    recommened added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const today = new Date().toISOString().slice(0, 10);
-
 const Navbar = () => {
   return (
     <header>
-      <nav className="navbar is-primary">
-        <div className="container  has-text-weight-bold">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item">
-              <span className="icon">
-                <i className="fas fa-meteor"></i>
+      <nav className='navbar is-primary'>
+        <div className='container  has-text-weight-bold'>
+          <div className='navbar-brand'>
+            <Link to='/' className='navbar-item'>
+              <span className='icon'>
+                <i className='fas fa-meteor'></i>
               </span>
               <span> Great Night Out</span>
             </Link>
-            <Link to="/events" className="navbar-item">
+            <Link to='/events' className='navbar-item'>
               Events
             </Link>
           </div>
-          <div className="navbar-end">
-            <Link to="/register" className="navbar-item">
+          <div className='navbar-end'>
+            <Link to='/register' className='navbar-item'>
               Sign up
             </Link>
-            <Link to="/login" className="navbar-item">
+            <Link to='/login' className='navbar-item'>
               Log in
             </Link>
           </div>
